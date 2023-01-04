4a1d3e40a8b7d8b580522b750bd436561c0f22dc
ashish bisht <ashish.bisht@techugo.com>
2023-01-04 13:56:55 +0530

    video upload added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import { Editor } from '@tinymce/tinymce-react';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
 function App() {
   const editorRef = useRef(null);
+  const [content, setContent] = useState("")
+  const [video,setVideo] = useState("")
   const log = () => {
     if (editorRef.current) {
       console.log(editorRef.current.getContent());
     }
   };
+  console.log(content)
   return (
     <div className="App">
-      <Editor
-
+     <Editor
         apiKey=""
-        initialValue="<p>This is the initial content of the editor</p>"
-        plugins="advlist autolink lists link image charmap print preview anchor
+        initialValue={content}
+        plugins="advlist autolink lists link image media charmap print preview anchor
                           searchreplace visualblocks code fullscreen
                           insertdatetime media table paste code help wordcount"
         init={{
@@ -24,7 +26,7 @@ function App() {
           toolbar:
             'undo redo | formatselect | h1 h2 h3 h4 h5 h6 | bold italic backcolor | \
                       alignleft aligncenter alignright alignjustify | \
-                      bullist numlist outdent indent | removeformat | image',
+                      bullist numlist outdent indent | removeformat | image | media',
           file_picker_callback: function (callback, value, meta) {
             // Provide file and text for the link dialog
             if (meta.filetype == 'file') {
@@ -41,12 +43,12 @@ function App() {
               callback('movie.mp4', { source2: 'alt.ogg', poster: 'image.jpg' });
             }
           },
-          file_picker_types: 'image',
+          file_picker_types: 'image media',
           /* and here's our custom image picker*/
           file_picker_callback: function (cb, value, meta) {
             var input = document.createElement('input');
             input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
+            input.setAttribute('accept', 'image/*,video/*');
 
             /*
               Note: In modern browsers input[type="file"] is functional without
@@ -60,30 +62,49 @@ function App() {
               var file = this.files[0];
 
               var reader = new FileReader();
-              reader.onload = function () {
+              reader.onload = async function () {
                 /*
                   Note: Now we need to register the blob in TinyMCEs image blob
                   registry. In the next release this part hopefully won't be
                   necessary, as we are looking to handle it internally.
                 */
-                var id = 'blobid' + (new Date()).getTime();
-                var blobCache = window.tinymce.activeEditor.editorUpload.blobCache;
-                var base64 = reader.result.split(',')[1];
-                var blobInfo = blobCache.create(id, file, base64);
-                blobCache.add(blobInfo);
+               debugger
+               let formData = new FormData();
+               formData.append("attachement",file);
+               let response = await axios.post("https://techugoapps.com:9099/uploadfile",formData)
+                // var id = 'blobid' + (new Date()).getTime();
+                // var blobCache = window.tinymce.activeEditor.editorUpload.blobCache;
+                // var base64 = reader.result.split(',')[1];
+                // var blobInfo = blobCache.create(id, file, base64);
+                // blobCache.add(blobInfo);
 
                 /* call the callback and populate the Title field with the file name */
-                cb(blobInfo.blobUri(), { title: file.name });
+                // cb(video, { title: file.name });
+                cb(response.data.result, { title: file.name });
+                // cb(video, { title: file.name });
+
               };
               reader.readAsDataURL(file);
             };
 
             input.click();
           },
+          video_template_callback: function(data) {
+            debugger
+            return '<video width="' + data.width + '" height="' + data.height + '"' + (data.poster ? ' poster="' + data.poster + '"' : '') + ' controls="controls">\n' + '<source src="' + data.source + '"' + (data.sourcemime ? ' type="' + data.sourcemime + '"' : '') + ' />\n' + (data.altsource ? '<source src="' + data.altsource + '"' + (data.altsourcemime ? ' type="' + data.altsourcemime + '"' : '') + ' />\n' : '') + '</video>';
+          }
         }}
+        
 
-        onChange={e => console.log(e)}
+        onChange={e => setContent(e.target.getContent())}
       />
+      {/* <input type="file" onChange={async e=>{
+        debugger
+        let formData = new FormData();
+        formData.append("attachement",e.target.files[0]);
+        let response = await axios.post("https://techugoapps.com:9099/uploadfile",formData)
+        setVideo(response.data.result);
+      }} value={content}/> */}
       <button onClick={log}>Log editor content</button>
     </div>
   );
